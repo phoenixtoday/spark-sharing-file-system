0a10b32d2fcf41c4a44b63c998208669d2d590dd
Phoenix <phoenixtoday@gmail.com>
2009-01-19 22:10:30 +0800

    first step of openfire plugin and added ant script for building openfire plugin

---
+++ openfire_src/edu/openfire/plugin/FileSystemPlugin.java
@@ -0,0 +1,22 @@
+package edu.openfire.plugin;
+
+import java.io.File;
+
+import org.jivesoftware.openfire.container.Plugin;
+import org.jivesoftware.openfire.container.PluginManager;
+
+public class FileSystemPlugin implements Plugin {
+
+	@Override
+	public void destroyPlugin() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void initializePlugin(PluginManager arg0, File arg1) {
+		System.out.println("INFO: File System Plugin Initialized");
+
+	}
+
+}
